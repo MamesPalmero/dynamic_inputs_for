10f92fc1026e53f71b10251024b8ff0a9f7de98f
Mamés Palmero <palmero.punzon@gmail.com>
2019-09-21 14:01:56 +0200

    Add package and deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule DynamicInputsFor.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "DynamicInputsFor",
+      description: "Phoenix view functions to add dynamism to forms with nested fields",
+      package: package()
     ]
   end
 
@@ -21,8 +24,15 @@ defmodule DynamicInputsFor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_html, "~> 2.13.3"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/MamesPalmero/dynamic_inputs_for"},
+      files: ~w(lib priv LICENSE mix.exs package.json README.md)
     ]
   end
 end
